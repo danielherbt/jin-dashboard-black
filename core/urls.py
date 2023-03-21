3339b3ad191d1be250a5fb0aaea079e9b3f1e19d
daniel <danielherbt@yahoo.es>
2023-03-21 18:05:11 -0500

    set .env

--- core/urls.py
+++ core/urls.py
@@ -28,24 +28,24 @@ from django.urls import path, include, re_path
 urlpatterns = [
     path("", include('admin_black.urls')),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
-    # Open API 자체를 조회 : json, yaml, 
-    path("docs/json/", SpectacularJSONAPIView.as_view(), name="schema-json"),
-    path("docs/yaml/", SpectacularYAMLAPIView.as_view(), name="swagger-yaml"),
-    # Open API Document UI로 조회: Swagger, Redoc
-    path("docs/swagger/", SpectacularSwaggerView.as_view(url_name="schema-json"), name="swagger-ui",),
-    path("docs/redoc/", SpectacularRedocView.as_view(url_name="schema-json"), name="redoc",),
+    # # Open API 자체를 조회 : json, yaml, 
+    # path("docs/json/", SpectacularJSONAPIView.as_view(), name="schema-json"),
+    # path("docs/yaml/", SpectacularYAMLAPIView.as_view(), name="swagger-yaml"),
+    # # Open API Document UI로 조회: Swagger, Redoc
+    # path("docs/swagger/", SpectacularSwaggerView.as_view(url_name="schema-json"), name="swagger-ui",),
+    # path("docs/redoc/", SpectacularRedocView.as_view(url_name="schema-json"), name="redoc",),
 
-    path('api/administracion/', include('api.manage.urls')),
-    path('api/escuelacobros/', include('api.schoolfees.urls')),
-    path('api/ventas/', include('api.sales.urls')),
-    path('api/documentos/', include('api.docs.urls')),
+    # path('api/administracion/', include('api.manage.urls')),
+    # path('api/escuelacobros/', include('api.schoolfees.urls')),
+    # path('api/ventas/', include('api.sales.urls')),
+    # path('api/documentos/', include('api.docs.urls')),
     
     # Leave `Home.Urls` as last the last line
     path("", include("home.urls"))
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]#+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns +=[
-    re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT,})
-] 
+#urlpatterns +=[
+#    re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT,})
+#] 
